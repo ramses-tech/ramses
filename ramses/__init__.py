8b44f66e8dc5558c8ddf97922663fda36d5f47df
Artem Kostiuk <postatum@gmail.com>
2015-04-09 13:04:06 +0300

    Add ability to turn off/on auth

--- ramses/__init__.py
+++ ramses/__init__.py
@@ -22,33 +22,35 @@ def includeme(config):
     # Set root factory
     config.root_factory = NefertariRootACL
 
-    # Enable authentication
+    # Process auth settings
     root = config.get_root_resource()
-    root.auth = Settings.asbool('auth', False)
+    root.auth = Settings.asbool('ramses.auth', False)
 
-    log.info('Configuring auth policies')
-    authz_policy = ACLAuthorizationPolicy()
-    config.set_authorization_policy(authz_policy)
+    if root.auth:
+        log.info('Configuring auth policies')
+        authz_policy = ACLAuthorizationPolicy()
+        config.set_authorization_policy(authz_policy)
 
-    from .auth import AuthUser
-    authn_policy = AuthTktAuthenticationPolicy(
-        Settings['auth_tkt_secret'],
-        callback=AuthUser.groupfinder,
-        hashalg='sha512',
-        cookie_name='ramses_auth_tkt',
-        http_only=True,
-    )
-    config.set_authentication_policy(authn_policy)
+        from .auth import AuthUser
+        authn_policy = AuthTktAuthenticationPolicy(
+            Settings['auth_tkt_secret'],
+            callback=AuthUser.groupfinder,
+            hashalg='sha512',
+            cookie_name='ramses_auth_tkt',
+            http_only=True,
+        )
+        config.set_authentication_policy(authn_policy)
 
     log.info('Parsing RAML and startign server generation')
     parsed_raml = pyraml.parser.load(
-        Settings['raml_schema'])
+        Settings['ramses.raml_schema'])
     generate_server(parsed_raml, config)
 
     log.info('Running nefertari.engine.setup_database')
     from nefertari.engine import setup_database
     setup_database(config)
 
-    config.include('ramses.auth', route_prefix='/auth')
+    if root.auth:
+        config.include('ramses.auth', route_prefix='/auth')
 
     log.info('Server succesfully generated\n')
