cb762f3c107955e0e78500b90195c4f9bb66f1a3
Artem Kostiuk <postatum@gmail.com>
2015-07-14 12:01:15 +0300

    Use new nefertari auth views

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -58,51 +58,33 @@ class TestSetupTicketPolicy(object):
         config = Mock()
         config.registry.settings = {'my_secret': 12345}
         config.registry.auth_model = Mock()
+        root = Mock()
+        config.get_root_resource.return_value = root
         auth._setup_ticket_policy(
             config=config, params={'secret': 'my_secret'})
-        config.add_route.assert_has_calls([
-            call('login', '/auth/login'),
-            call('logout', '/auth/logout'),
-            call('register', '/auth/register'),
-        ])
-        login, logout, register = config.add_view.call_args_list
+        assert root.add.call_count == 3
+        login, logout, register = root.add.call_args_list
         login_kwargs = login[1]
         assert sorted(login_kwargs.keys()) == sorted([
-            'request_method', 'view', 'route_name', 'attr'])
-        assert login_kwargs['route_name'] == 'login'
-        assert login_kwargs['attr'] == 'login'
-        assert login_kwargs['request_method'] == 'POST'
+            'view', 'prefix', 'factory'])
+        assert login_kwargs['prefix'] == 'auth'
+        assert login_kwargs['factory'] == 'nefertari.acl.AuthenticationACL'
 
         logout_kwargs = logout[1]
         assert sorted(logout_kwargs.keys()) == sorted([
-            'view', 'route_name', 'attr'])
-        assert logout_kwargs['route_name'] == 'logout'
-        assert logout_kwargs['attr'] == 'logout'
+            'view', 'prefix', 'factory'])
+        assert logout_kwargs['prefix'] == 'auth'
+        assert logout_kwargs['factory'] == 'nefertari.acl.AuthenticationACL'
 
         register_kwargs = register[1]
         assert sorted(register_kwargs.keys()) == sorted([
-            'request_method', 'view', 'route_name', 'attr'])
-        assert register_kwargs['route_name'] == 'register'
-        assert register_kwargs['attr'] == 'register'
-        assert register_kwargs['request_method'] == 'POST'
+            'view', 'prefix', 'factory'])
+        assert register_kwargs['prefix'] == 'auth'
+        assert register_kwargs['factory'] == 'nefertari.acl.AuthenticationACL'
 
 
 @pytest.mark.usefixtures('engine_mock')
 class TestSetupApiKeyPolicy(object):
-
-    @patch('ramses.auth.ApiKeyAuthenticationPolicy')
-    def test_policy_params(self, mock_policy):
-        from ramses import auth
-        auth_model = Mock()
-        config = Mock()
-        config.registry.auth_model = auth_model
-        policy = auth._setup_apikey_policy(config, {'foo': 'bar'})
-        mock_policy.assert_called_once_with(
-            foo='bar', check=auth_model.get_groups_by_token,
-            credentials_callback=auth_model.get_token_credentials,
-            user_model=auth_model,
-        )
-        assert policy == mock_policy()
 
     @patch('ramses.auth.ApiKeyAuthenticationPolicy')
     def test_policy_params(self, mock_policy):
@@ -124,34 +106,28 @@ class TestSetupApiKeyPolicy(object):
         auth_model = Mock()
         config = Mock()
         config.registry.auth_model = auth_model
+        root = Mock()
+        config.get_root_resource.return_value = root
         auth._setup_apikey_policy(config, {})
-        config.add_route.assert_has_calls([
-            call('token', '/auth/token'),
-            call('reset_token', '/auth/reset_token'),
-            call('register', '/auth/register'),
-        ])
-
-        token, reset_token, register = config.add_view.call_args_list
+        assert root.add.call_count == 3
+        token, reset_token, register = root.add.call_args_list
         token_kwargs = token[1]
         assert sorted(token_kwargs.keys()) == sorted([
-            'request_method', 'view', 'route_name', 'attr'])
-        assert token_kwargs['route_name'] == 'token'
-        assert token_kwargs['attr'] == 'claim_token'
-        assert token_kwargs['request_method'] == 'POST'
+            'view', 'prefix', 'factory'])
+        assert token_kwargs['prefix'] == 'auth'
+        assert token_kwargs['factory'] == 'nefertari.acl.AuthenticationACL'
 
         reset_token_kwargs = reset_token[1]
         assert sorted(reset_token_kwargs.keys()) == sorted([
-            'request_method', 'view', 'route_name', 'attr'])
-        assert reset_token_kwargs['route_name'] == 'reset_token'
-        assert reset_token_kwargs['attr'] == 'reset_token'
-        assert reset_token_kwargs['request_method'] == 'POST'
+            'view', 'prefix', 'factory'])
+        assert reset_token_kwargs['prefix'] == 'auth'
+        assert reset_token_kwargs['factory'] == 'nefertari.acl.AuthenticationACL'
 
         register_kwargs = register[1]
         assert sorted(register_kwargs.keys()) == sorted([
-            'request_method', 'view', 'route_name', 'attr'])
-        assert register_kwargs['route_name'] == 'register'
-        assert register_kwargs['attr'] == 'register'
-        assert register_kwargs['request_method'] == 'POST'
+            'view', 'prefix', 'factory'])
+        assert register_kwargs['prefix'] == 'auth'
+        assert register_kwargs['factory'] == 'nefertari.acl.AuthenticationACL'
 
 
 @pytest.mark.usefixtures('engine_mock')
