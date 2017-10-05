701da151e544fdd0944b81f0be684f97dae20c9d
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2017-10-05 15:58:41 -0700

    Updated copyright info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,8 +54,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Ramses'
-copyright = u'2015, Brandicted'
-author = u'Brandicted'
+copyright = u'Ramses Tech'
+author = u'Ramses Tech, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
